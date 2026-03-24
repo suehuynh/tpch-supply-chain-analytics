6a9a7e3224a6a27050476cef20c05218d5e964ea
Sue Huynh <58705955+suehuynh@users.noreply.github.com>
2026-03-24 19:00:17 +0000

    Python scripts to load data for bronze layer

---
+++ scripts/load_orders.py
@@ -0,0 +1,6 @@
+import duckdb
+
+def create_dataset(table_name="orders", source_database="tpch.db"):
+    con = duckdb.connect(source_database)
+    pulled_df = con.sql(f"select * from {table_name}").pl()
+    return pulled_df.rename(lambda colname: colname[2:])
