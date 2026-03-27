872e9dc951c330b925a72ce454b53f3b04674c7b
Sue Huynh <58705955+suehuynh@users.noreply.github.com>
2026-03-27 13:24:04 +0000

    Update dashboard

--- models/generate_dashboard.py
+++ models/generate_dashboard.py
@@ -4,8 +4,9 @@ import pandas
 
 def generate_dashboard():
     with duckdb.connect("tpch.db", read_only=True) as con:
-        df_nation = con.sql("SELECT * FROM gold.agg_nation").pl()
-        df_supplier = con.sql("SELECT * FROM gold.agg_supplier").pl()
+        df_nation = con.sql("SELECT * FROM gold.agg_nation ORDER BY avg_days DESC LIMIT 10").pl()
+        df_supplier = con.sql("SELECT * FROM gold.agg_supplier ORDER BY avg_days DESC LIMIT 10").pl()
+        df_shipmode = con.sql("SELECT * FROM gold.agg_shipmode ORDER BY avg_days DESC LIMIT 10").pl()
 
     # Convert to HTML 
     html_content = f"""
@@ -15,7 +16,9 @@ def generate_dashboard():
             <h1>Supply Chain Dashboard</h1>
             <h2>Avg Shipping by Nation</h2>
             {df_nation.to_pandas().to_html(classes='table table-striped')}
-            <h2>Slowest Suppliers</h2>
+            <h2>Avg Shipping by Shipping Mode</h2>
+            {df_supplier.to_pandas().to_html(classes='table table-dark')}
+            <h2>SAvg Shipping by Suppliers</h2>
             {df_supplier.to_pandas().to_html(classes='table table-dark')}
         </body>
     </html>
