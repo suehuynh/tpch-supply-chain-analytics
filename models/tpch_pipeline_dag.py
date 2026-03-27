722be37a8064f5e174b6735533f74763bdadbcf2
Sue Huynh <58705955+suehuynh@users.noreply.github.com>
2026-03-27 02:13:50 +0000

    Update pipeline to include visualization

--- models/tpch_pipeline_dag.py
+++ models/tpch_pipeline_dag.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.decorators import task
-from airflow.operators.dummy import DummyOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 from run_pipeline import create_shipping_metrics
@@ -15,15 +15,15 @@ default_args = {
 
 with DAG(
     'tpch_pipeline_dag',
-    description='Demo DAG',
+    description='Supply Chain DAG',
     default_args=default_args,
-    schedule_interval=timedelta(minutes=1),
+    schedule=timedelta(minutes=5),
     start_date=datetime(2026, 3, 25),
     catchup=False,
 ) as dag:
     @task 
     def create_shipping_metrics_task():
         create_shipping_metrics()
-    stop_pipeline = DummyOperator(task_id='stop_pipeline')
+    stop_pipeline = EmptyOperator(task_id='stop_pipeline')
 
     create_shipping_metrics_task() >> stop_pipeline
