89c7b005ab3071000d6de1b4d942814d23508c35
Sue Huynh <58705955+suehuynh@users.noreply.github.com>
2026-03-27 02:13:00 +0000

    Edit scripts to run pre-aggregated tables separately

---
+++ models/etl/gold/pre_aggregated/metrics_by_supplier.py
@@ -0,0 +1,19 @@
+import json
+import polars as pl
+import duckdb
+
+# from utils.metadata import get_latest_run_metrics
+
+def create_dataset(wide_supplier):
+    wide_shipping_length = wide_supplier.with_columns(
+        (pl.col("shipdate") - pl.col("orderdate")).dt.total_days().alias("shipping_length")
+    )
+
+    # Aggregate by Supplier
+    agg_supplier = wide_shipping_length.group_by("supplier_name").agg([
+        pl.col("shipping_length").max().alias("max_days"),
+        pl.col("shipping_length").min().alias("min_days"),
+        pl.col("shipping_length").mean().alias("avg_days")
+    ]).sort("avg_days")
+
+    return agg_supplier
